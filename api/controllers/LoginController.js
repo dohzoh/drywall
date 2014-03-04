55bd3b1cf1f1ff63141b28733c0997ac6b6e07de
dozo <dohzoh@outlook.com>
2014-03-04 22:41:36 +0900

    add contact, login, signup

---
+++ api/controllers/LoginController.js
@@ -0,0 +1,57 @@
+/**
+ * LoginController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+  /**
+   * Action blueprints:
+   *    `/login/index`
+   *    `/login`
+   */
+   index: function (req, res) {
+    
+    // Send a JSON response
+    return res.view({
+      hello: 'world'
+    });
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/login/forgot`
+   */
+   forgot: function (req, res) {
+    
+    // Send a JSON response
+    return res.view({
+      hello: 'world'
+    });
+  },
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to LoginController)
+   */
+  _config: {}
+
+  
+};
